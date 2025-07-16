0b8a9eb6ddf9e4159f63bc8f7458d7e10f660d5b
alfasetyo <89786833+alfasetyo@users.noreply.github.com>
2025-07-17 00:05:59 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import { useState } from 'react'
-import { supabase } from '../lib/supabaseClient'
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+
 
 export default function Home() {
   const [url, setUrl] = useState('')
